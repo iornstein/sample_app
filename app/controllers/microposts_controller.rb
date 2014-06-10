5c3eb0be52828b8fcfd2669fffe872c4b3e6e864
Ian Ornstein <ian.ornstein@gmail.com>
2014-06-09 22:26:11 -0700

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,7 +13,9 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy
+ def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
@@ -20,4 +23,10 @@ class MicropostsController < ApplicationController
     def micropost_params
       params.require(:micropost).permit(:content)
     end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+
 end
